ebc78e4731b53aa217901170409f11c08c3212d9
DoDo <DoDo.com>
2022-07-12 14:05:42 +0800

    hhh

--- async/basic.js
+++ async/basic.js
@@ -2,12 +2,18 @@
     1、无return值。用Promise.resolve()包装undefined
     2、有return值。用Promise.resolve()包装该值
     */
-    /* await语句暂停async函数，等待其参数promise的结果：
+/* await语句暂停async函数，等待其参数promise的结果：
     1、resolved。继续执行async函数。返回value。
     2、rejected。抛出错误，中断async函数。
     */
-    async function test(){
-      /* return await Promise.resolve("success") */
-      return await Promise.reject(new Error("fail"))  //等价于throw new Error("fail")
-    }
-    test().then((value)=>{console.log(value)}).catch((err)=>{console.log(err)})
+async function test() {
+  /* return await Promise.resolve("success") */
+  return await Promise.reject(new Error("fail")); //等价于throw new Error("fail")
+}
+test()
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
