6d1e18fd83f93ee7b2b69109393f1ebfb04d21fc
DoDo <DoDo.com>
2022-11-24 19:39:48 +0800

    mm

--- vue3_vant/src/App.vue
+++ vue3_vant/src/App.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-
+  <div class="container"></div>
 </template>
 
 <style scoped>
-
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+  background-image: url(https://tse3-mm.cn.bing.net/th/id/OIP-C.QnObsMl_X8Ij4K1Bi8f_DwHaNJ?w=170&h=303&c=7&r=0&o=5&dpr=2&pid=1.7);
+  background-size: 100vw 100vh;
+}
 </style>
