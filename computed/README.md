387b817592bbdcde5c767ba89587b2bb7871e6eb
DoDo <DoDo.com>
2022-11-26 09:07:03 +0800

    ok

--- computed/README.md
+++ computed/README.md
@@ -1,4 +1,4 @@
-# moban
+# reg
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- computed/src/components/Register.vue
+++ computed/src/components/Register.vue
@@ -1,9 +1,9 @@
 <script setup>
-import MoBan from './components/MoBan.vue'
+
 </script>
 
 <template>
-  <MoBan></MoBan>
+
 </template>
 
 <style scoped>
